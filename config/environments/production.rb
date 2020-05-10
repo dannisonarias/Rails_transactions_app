bccbd0e57910da7362025ac389f19f18242a691d
DannisonArias <dannisonarias@gmail.com>
2020-05-10 18:07:16 -0400

    fix image on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.compile = true
+  config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -29,7 +30,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
