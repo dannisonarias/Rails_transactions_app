9f638278a125905a1a4005aedcd9985ea93bba9d
DannisonArias <dannisonarias@gmail.com>
2020-05-13 10:54:39 -0400

    update devise & sidebar hover color

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
   has_many :groups
   has_many :transactions, dependent: :destroy
   has_one_attached :image
